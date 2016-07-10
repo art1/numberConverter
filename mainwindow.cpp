efb519469306249d9585f42c96250c74a8991ee1
art1 <asscience22@gmail.com>
2016-07-10 13:45:25 +0200

    added float to byte conversion for floating point numbers and reverse; now with better binary readability

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,17 +2,14 @@
 #include "ui_mainwindow.h"
 #include "qdebug.h"
 
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    float one = 1.0f;
-    uint8_t tmp[4];
-    floatToChar(tmp,one);
-    qDebug()<<tmp[0]<<tmp[1]<<tmp[2]<<tmp[3];
-
 }
+
 
 void MainWindow::floatToChar(uint8_t* target, float number){
     char *tmp = (char *) &number;
@@ -22,10 +19,12 @@ void MainWindow::floatToChar(uint8_t* target, float number){
     }
 }
 
+
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
 
 //binary stuff changed from GUI
 void MainWindow::on_lineEdit_bin_textEdited(const QString &arg1)
@@ -45,6 +44,7 @@ void MainWindow::on_lineEdit_bin_textEdited(const QString &arg1)
     }
 }
 
+
 // hex stuff changed from GUI
 void MainWindow::on_lineEdit_hex_textEdited(const QString &arg1)
 {
@@ -54,13 +54,26 @@ void MainWindow::on_lineEdit_hex_textEdited(const QString &arg1)
     } else {
         QString tmp = arg1.toUpper();
         int val = tmp.toInt(&ok,16);
-        ui->lineEdit_bin->setText((tmp.setNum(val,2)));
-        ui->lineEdit_dec->setText(QString::number(val));
-        QChar c(val);
-        if(val >=0 && val <=255)ui->lineEdit_ascii->setText(c);
-        else ui->lineEdit_ascii->setText("n/a");
+        tmp.setNum(val,2);
+        // prepend zeroes for better byte readability
+        while(((tmp.size() % 8) < 8) && ((tmp.size() % 8) > 0)) tmp.prepend("0");
+        for(int i=tmp.size();i>0;i-=8) tmp.insert(i, " ");
+
+        if(!(ui->lineEdit_hex->text() == "")){
+            ui->lineEdit_bin->setText(tmp);
+            ui->lineEdit_dec->setText(QString::number(val));
+            QChar c(val);
+            if(val >=0 && val <=255) ui->lineEdit_ascii->setText(c);
+            else ui->lineEdit_ascii->setText("n/a");
+        } else {
+            ui->lineEdit_ascii->clear();
+            ui->lineEdit_bin->clear();
+            ui->lineEdit_dec->clear();
+            ui->lineEdit_float->clear();
+        }
     }
 }
+
 
 //dec stuff changed from GUI
 void MainWindow::on_lineEdit_dec_textEdited(const QString &arg1)
@@ -69,39 +82,55 @@ void MainWindow::on_lineEdit_dec_textEdited(const QString &arg1)
     if(ui->signed_2->isChecked()){
 
     } else {
-        //        ui->lineEdit_hex->setText(QString::number(arg1.toInt(&ok,16)));
+        QString u = "";
         int val = arg1.toInt(&ok);
         if(ok){
-            ui->lineEdit_bin->setText(QString::number(val,2));
+            QString temp = QString::number(val,2);
+            while(((temp.size() % 8) < 8) && ((temp.size() % 8) > 0)) temp.prepend("0");
+            for(int i=temp.size();i>0;i-=8) temp.insert(i, " ");
+            ui->lineEdit_bin->setText(temp);
             ui->lineEdit_hex->setText(QString::number(val,16));
+
             QChar c(val);
             if(val >=0 && val <=255)ui->lineEdit_ascii->setText(c);
             else ui->lineEdit_ascii->setText("n/a");
+
             // convert integer to float representatoin
             float t = (float)val;
             char *tmp = (char *) &t;
-            QString u = "";
             for(int i=0;i<4;i++){
                 u.append(QString::number((uint8_t)tmp[i]));
                 if(tmp[i] == 0) u.append("0");
                 u.append(" ");
             }
-            ui->lineEdit_float->setText(u);
+        } else {
+            float val = arg1.toFloat(&ok);
+            if(ok){
+                // convert float to float representatoin in bytes
+                char *tmp = (char *) &val;
+                for(int i=0;i<4;i++){
+                    u.append(QString::number((uint8_t)tmp[i]));
+                    if(tmp[i] == 0) u.append("0");
+                    u.append(" ");
+                }
+            }
+            // set other vals to n/a or zero, cause floats in hex/bin -> nah not needed now
+            ui->lineEdit_ascii->setText("n/a");
+            ui->lineEdit_bin->clear();
+            ui->lineEdit_hex->clear();
         }
-
-
+        ui->lineEdit_float->setText(u);
     }
 }
+
 
 // char stuff
 void MainWindow::on_lineEdit_ascii_textEdited(const QString &arg1)
 {
-    bool ok;
     if(ui->signed_2->isChecked()){
 
     } else {
         if(arg1.length() > 0){
-            QChar c = arg1.at(0);
             int val = arg1.at(0).toLatin1();
             ui->lineEdit_bin->setText(QString::number(val,2));
             ui->lineEdit_hex->setText(QString::number(val,16));
@@ -115,10 +144,25 @@ void MainWindow::on_lineEdit_ascii_textEdited(const QString &arg1)
 // int to float as byte representation!
 void MainWindow::on_lineEdit_float_textEdited(const QString &arg1)
 {
-    bool ok;
     if(ui->signed_2->isChecked()){
 
     }else{
+        QStringList vals = ui->lineEdit_float->text().split(QRegExp("\\s"));
+        if(vals.size() == 4){
+            bool ok = true;
+            bool tmp = false;
+            // check if input valid bytes
+            for(int i=0;i<4;i++){
+                if(vals.at(i).toInt(&tmp) >= 255) tmp = false;
+                ok &= tmp;
+            }
+            if(ok){
+                float out;
+                uint8_t * ptr = (uint8_t *) &out;
+                for(int i=0;i<4;i++) ptr[i] = vals.at(i).toInt();
+                ui->lineEdit_dec->setText(QString::number(out));
+            } else ui->lineEdit_dec->clear();
+        }
 
     }
 }
